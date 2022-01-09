ff7841aa8e285fceb1e889328845c3b3fdcb639e
Yusaf Osei-Bonsu <yusaf.oseibonsu@centrica.com>
2022-01-09 21:01:21 +0000

    Replaced 'class' with 'className'

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -5,13 +5,13 @@ export class Track extends React.Component {
 
     render () {
         return (
-            <div class="Track">
-                    <div class="Track-information">
+            <div className="Track">
+                    <div className="Track-information">
                         <p>
                             {/* track artist will go here | track album will go here */}
                         </p>
                     </div>
-                    <button class="Track-action">
+                    <button className="Track-action">
                         {/* + or - will go here */}
                     </button>
             </div>
