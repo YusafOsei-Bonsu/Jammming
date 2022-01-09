10f4df42fa0ebbb6c23c6490f8c0fddaea3aa5d8
Yusaf Osei-Bonsu <yusaf.oseibonsu@centrica.com>
2022-01-09 21:08:14 +0000

    Replaced value with defaultValue for input element

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -7,7 +7,7 @@ export class Playlist extends React.Component {
     render () {
         return (
             <div className="Playlist">
-                <input value="New Playlist"/>
+                <input defaultValue={"New Playlist"}/>
                 <TrackList />
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
