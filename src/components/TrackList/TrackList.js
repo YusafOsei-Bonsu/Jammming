5965ec5fb1e7d0ffde4b36ab4ef9fb55763585ab
Yusaf Osei-Bonsu <yusaf.oseibonsu@centrica.com>
2022-01-15 17:53:22 +0000

    3 hardcoded Track instances

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -7,9 +7,9 @@ class TrackList extends React.Component {
     render () {
         return (
             <div className="TrackList">
-                {this.props.tracks.map((track) => {
-                    return <Track key={track.id} />
-                })}
+                <Track />
+                <Track />
+                <Track />
             </div>
         );
     }
