3ab5ded767f608dcd3b6b31224dd0fb4df0d82d5
Yusaf Osei-Bonsu <yusaf.oseibonsu@centrica.com>
2022-01-16 16:52:52 +0000

    handleNameChange()

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -3,11 +3,20 @@ import TrackList from "../TrackList/TrackList.js";
 import "./Playlist.css";
 
 export class Playlist extends React.Component {
+
+    constructor (props) {
+        super(props);
+    }
+
+    handleNameChange = (e) => {
+        this.props.onNameChange(e.target.value);
+    }
+ 
     render () {
         let { playlistTracks, onRemove } = this.props;
         return (
             <div className="Playlist">
-                <input defaultValue={"New Playlist"}/>
+                <input defaultValue={"New Playlist"} onChange={this.handleNameChange}/>
                 <TrackList tracks={playlistTracks} onRemove={onRemove} isRemoval={true} />
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
