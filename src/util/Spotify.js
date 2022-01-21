7a18df98ca73f09cfd11a944a4067574feeb61fc
Yusaf Osei-Bonsu <yusaf.oseibonsu@centrica.com>
2022-01-21 22:16:07 +0000

    Completed getAccessToken() function and emptied search() method

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,28 +1,31 @@
-let accessToken = "";
-export default {
-    getAccessToken: function () {
+const clientID = "25386ade3c4c4f59a54769365f763e08";
+const redirectURI = "http://localhost:3000/";
+const spotifyURL = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+let accessToken = undefined;
+let expiryIn = undefined;
+
+const Spotify = {
+    getAccessToken () {
         if (accessToken) {
             return accessToken;
+        } else {
+            let urlAccessToken = window.location.href.match(/access_token=([^&]*)/);
+            let urlExpiryIn = window.location.href.match(/expires_in=([^&]*)/);
+            // Checking the existence of the access token and expiry time inside the url
+            if (urlAccessToken && urlExpiryIn) {
+                accessToken = urlAccessToken[1];
+                expiryIn = Number(urlExpiryIn[1]);
+                // Clear the parameters from the URL, so the app doesn’t try grabbing the access token after it has expired
+                window.setTimeout(() => accessToken = '', expiresIn * 1000);
+                window.history.pushState('Access Token', null, '/');
+            } else {
+                window.location = spotifyURL;
+            }
         }
     },
-    search: async function (term) {
-        return new Promise((resolve) => {
-            let response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`
-                }
-            });
-            let data = await response.json();
-            let tracks = data.map(function (track) {
-                return {
-                    id: track.id,
-                    name: track.name,
-                    artist: track.artists[0].name,
-                    album: track.album.name,
-                    uri: track.uri
-                };
-            });
-            resolve(tracks);
-        });
+    search (term) {
+        
     }
 };
+
+export default Spotify;
