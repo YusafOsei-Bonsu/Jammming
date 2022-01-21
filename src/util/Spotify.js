48592b8ee08c81c4da920277d5d26b86b78e9230
Yusaf Osei-Bonsu <yusaf.oseibonsu@centrica.com>
2022-01-21 08:50:24 +0000

    search()

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -3,8 +3,26 @@ export default {
     getAccessToken: function () {
         if (accessToken) {
             return accessToken;
-        } else {
-            // 
         }
+    },
+    search: async function (term) {
+        return new Promise((resolve) => {
+            let response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+            let data = await response.json();
+            let tracks = data.map(function (track) {
+                return {
+                    id: track.id,
+                    name: track.name,
+                    artist: track.artists[0].name,
+                    album: track.album.name,
+                    uri: track.uri
+                };
+            });
+            resolve(tracks);
+        });
     }
 };
