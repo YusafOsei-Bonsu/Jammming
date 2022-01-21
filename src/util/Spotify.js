792275cafa645c19fbe463aeeb66c9ba875afc9e
Yusaf Osei-Bonsu <yusaf.oseibonsu@centrica.com>
2022-01-21 22:55:51 +0000

    Installed & imported axios

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 const clientID = "25386ade3c4c4f59a54769365f763e08";
 const redirectURI = "http://localhost:3000/";
 const spotifyURL = `https://accounts.spotify.com/authorize?client_id=${encodeURIComponent(clientID)}&response_type=token&scope=playlist-modify-public&redirect_uri=${encodeURIComponent(redirectURI)}`;
