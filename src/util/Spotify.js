8e1fe690460131d88f1fb3376e3abf8871a39574
Yusaf Osei-Bonsu <yusaf.oseibonsu@centrica.com>
2022-01-22 10:53:43 +0000

    Changed redirect uri to surge url

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const clientID = "25386ade3c4c4f59a54769365f763e08";
-const redirectURI = "http://localhost:3000/";
+const redirectURI = "http://guarded-ground.surge.sh/";
 const spotifyURL = `https://accounts.spotify.com/authorize?client_id=${encodeURIComponent(clientID)}&response_type=token&scope=playlist-modify-public&redirect_uri=${encodeURIComponent(redirectURI)}`;
 const spotifyWebAPI = "https://api.spotify.com/v1";
 let accessToken = undefined;
